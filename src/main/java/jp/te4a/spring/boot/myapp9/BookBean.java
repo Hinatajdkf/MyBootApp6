e196e3038afc8497ef2d060a2244b6b48dadc8c1
Hinata Takeda <210011@jc-21.jp>
2024-05-24 15:04:57 +0900

    更新

--- src/main/java/jp/te4a/spring/boot/myapp9/BookBean.java
+++ src/main/java/jp/te4a/spring/boot/myapp9/BookBean.java
@@ -1,14 +1,24 @@
-package jp.te4a.spring.boot.myapp8;
+package jp.te4a.spring.boot.myapp9;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import javax.persistence.*;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+@Table(name="books")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor//引数なしコンストラクタ自動生成
 //DB(テーブル)と連携
 public class BookBean {
+    @Id
+    @GeneratedValue
+    @Column(nullable = false)
     private Integer id;
     private String title;
     private String writer;
